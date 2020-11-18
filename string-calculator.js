952879bae8e49d30434753c8b8c70b70797de847
Robyn Heslop <robyn.g.heslop@gmail.com>
2020-11-18 21:02:21 +0800

    refactor

--- string-calculator.js
+++ string-calculator.js
@@ -5,11 +5,11 @@ const stringCalculator = {
         const separatedLinesOfString = params.split("\n");
         if (separatedLinesOfString[0].substring(0, 2) === "//") {
             const separator = separatedLinesOfString[0].substring(2);
-            const calculationString = params.substring(separatedLinesOfString[0].length + 1)
-            splitStringBySeparator = calculationString.split(separator);
+            const stringAfterSeparator = params.substring(separatedLinesOfString[0].length + 1)
+            splitStringBySeparator = stringAfterSeparator.split(separator);
         } else {
-            const noNewLinesInString = params.replace("\n", ",");
-            splitStringBySeparator = noNewLinesInString.split(",");
+            const removeLineBreaksInString = params.replace("\n", ",");
+            splitStringBySeparator = removeLineBreaksInString.split(",");
         }
         const arrayOfNumberValues = splitStringBySeparator.map(number => parseInt(number));
         const filteredOutLargeNumbers = arrayOfNumberValues.filter(number => number <= 1000);
