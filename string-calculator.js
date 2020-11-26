03995d7403276499c0948cddcc6ca091e094973a
Robyn Heslop <robyn.g.heslop@gmail.com>
2020-11-26 20:32:34 +0800

    receives '1', returns 1

--- string-calculator.js
+++ string-calculator.js
@@ -1,5 +1,6 @@
 const stringCalculator = params => {
     if (!params) return 0;
+    return parseInt(params);
 };
 
 export default stringCalculator;
--- string-calculator.test.js
+++ string-calculator.test.js
@@ -1,7 +1,10 @@
 import stringCalculator from "./string-calculator";
 
 describe("string-calculator", () => {
-    it("receives an string, returns 0", () => {
+    it("receives an empty string, returns 0", () => {
         expect(stringCalculator("")).toBe(0);
+    });
+    it("receives '1', returns 1", () => {
+        expect(stringCalculator("1")).toBe(1);
     });
 });
